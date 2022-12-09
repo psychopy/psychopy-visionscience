c8a61f5d481e352f5289aeaf1f3f40cf0ba18484
mdcutone <matthew.cutone@gmail.com>
2022-12-09 10:24:57 -0500

    DOCS: Added header to component package

--- psychopy_visionscience/components/__init__.py
+++ psychopy_visionscience/components/__init__.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Originally from the PsychoPy library
+# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
+# Distributed under the terms of the GNU General Public License (GPL).
+
+"""Components for Builder associated with the various stimuli classes included
+in the main package.
+"""
+
 from .noise import NoiseStimComponent
 from .envelopegrating import EnvGratingComponent
